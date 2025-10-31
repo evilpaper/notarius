0ac52c3f439edeec2937a23c06cea1a046651eea
Pelle Lundgren <pellburt@gmail.com>
2025-10-31 06:43:23 +0100

    feat: render out uploaded images on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import fs from "node:fs/promises";
 import { removeFile } from "../remove-action";
+import { FileIcon } from "lucide-react";
 
 export default async function AdminPage() {
   const files = await fs.readdir("./public/uploads");
   const images = files
     .filter((file) => file.endsWith(".webp"))
+    .map((file) => `/uploads/${file}`);
+  const pdfs = files
+    .filter((file) => file.endsWith(".pdf"))
     .map((file) => `/uploads/${file}`);
 
   return (
@@ -27,7 +31,25 @@ export default async function AdminPage() {
                 <form action={removeFile}>
                   <input type="hidden" name="filename" value={image} />
                   <button
-                    className="border-gray-700 rounded-md p-2 text-sm w-full"
+                    className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
+                    type="submit"
+                  >
+                    Remove
+                  </button>
+                </form>
+              </li>
+            ))}
+            {pdfs.map((pdf) => (
+              <li
+                key={pdf}
+                className="px-2 h-auto flex flex-col items-center gap-2"
+              >
+                <FileIcon size={16} />
+                <p>{pdf}</p>
+                <form action={removeFile}>
+                  <input type="hidden" name="filename" value={pdf} />
+                  <button
+                    className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
                     type="submit"
                   >
                     Remove
