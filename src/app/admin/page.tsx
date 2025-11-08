c02e0778048e9c7252fb60694ea4edb2d192bd43
Pelle Lundgren <pellburt@gmail.com>
2025-11-08 15:34:48 +0100

    feat: show file name for both images and pdfs on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,10 @@ import { removeFile } from "../remove-action";
 import { FileIcon } from "lucide-react";
 
 export default async function AdminPage() {
+  // This is only really the file URL, not the actual files
   const files = await fs.readdir("./public/uploads");
+
+  // Should probably be called image URLs etc,
   const images = files
     .filter((file) => file.endsWith(".webp"))
     .map((file) => `/uploads/${file}`);
@@ -18,45 +21,52 @@ export default async function AdminPage() {
         <p>Uploaded files</p>
         {images.length > 0 ? (
           <ul className="flex gap-4">
-            {images.map((image) => (
-              <li key={image} className="px-2 h-auto ">
-                <Image
-                  key={image}
-                  src={image}
-                  width={400}
-                  height={400}
-                  alt={image}
-                  className="object-cover w-full"
-                />
-                <form action={removeFile}>
-                  <input type="hidden" name="filename" value={image} />
-                  <button
-                    className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
-                    type="submit"
-                  >
-                    Remove
-                  </button>
-                </form>
-              </li>
-            ))}
-            {pdfs.map((pdf) => (
-              <li
-                key={pdf}
-                className="px-2 h-auto flex flex-col items-center gap-2"
-              >
-                <FileIcon size={16} />
-                <p>{pdf}</p>
-                <form action={removeFile}>
-                  <input type="hidden" name="filename" value={pdf} />
-                  <button
-                    className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
-                    type="submit"
-                  >
-                    Remove
-                  </button>
-                </form>
-              </li>
-            ))}
+            {images.map((image) => {
+              const filename = image.split("/").pop();
+              return (
+                <li key={image} className="px-2 h-auto ">
+                  <Image
+                    key={image}
+                    src={image}
+                    width={400}
+                    height={400}
+                    alt={image}
+                    className="object-cover w-full"
+                  />
+                  <p className="text-sm text-center py-2">{filename}</p>
+                  <form action={removeFile}>
+                    <input type="hidden" name="filename" value={image} />
+                    <button
+                      className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
+                      type="submit"
+                    >
+                      Remove
+                    </button>
+                  </form>
+                </li>
+              );
+            })}
+            {pdfs.map((pdf) => {
+              const filename = pdf.split("/").pop();
+              return (
+                <li
+                  key={pdf}
+                  className="px-2 h-auto flex flex-col items-center gap-2"
+                >
+                  <FileIcon size={16} />
+                  <p className="text-sm text-center py-2">{filename}</p>
+                  <form action={removeFile}>
+                    <input type="hidden" name="filename" value={pdf} />
+                    <button
+                      className="border-gray-700 rounded-md p-2 text-sm w-full hover:bg-gray-100"
+                      type="submit"
+                    >
+                      Remove
+                    </button>
+                  </form>
+                </li>
+              );
+            })}
           </ul>
         ) : (
           <p className="text-sm italic text-muted-foreground">
